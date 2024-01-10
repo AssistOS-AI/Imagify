c4975b3e383147db4124445a9e5b5fdae88905d2
Koroem <conduratvictor@yahoo.com>
2024-01-10 11:01:24 +0200

    service update

--- services/routingService.js
+++ services/routingService.js
@@ -3,11 +3,15 @@ export class routingService {
     async navigateToLocation(locationArray = [], appName) {
         const IMAGE_BRAINSTORMING_PAGE = "image-brainstorming-page";
 
-        if (locationArray.length === 0 || locationArray[0] !== IMAGE_BRAINSTORMING_PAGE) {
-            console.error("Invalid URL structure: URL must start with 'image-brainstorming-page'");
+        if (locationArray.length === 0 || locationArray[0] === IMAGE_BRAINSTORMING_PAGE) {
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${CHATBOTS_PAGE}`;
+            await webSkel.changeToDynamicPage(CHATBOTS_PAGE, pageUrl);
             return;
         }
-
+ if(locationArray[locationArray.length-1]!== IMAGE_BRAINSTORMING_PAGE){
+         console.error(`Invalid URL: URL must end with ${IMAGE_BRAINSTORMING_PAGE}`);
+            return;
+        }
         const webComponentName = locationArray[locationArray.length - 1];
         const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${locationArray.join("/")}`;
         await webSkel.changeToDynamicPage(webComponentName, pageUrl);
